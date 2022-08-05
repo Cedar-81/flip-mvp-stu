8d06e7dc9bd62309f6e88a8e296d406ea54700a9
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-08-05 09:03:56 -1100

    bug fix

--- components/layout.js
+++ components/layout.js
@@ -44,12 +44,11 @@ function Layout({ children }) {
   });
   console.log(data);
 
+  refetch();
   if (data && !router.pathname.includes("/auth")) {
     if (data.auth === "authorized") {
       setIsAuth(true);
     } else if (data.auth === "unauthorized") {
-      refetch();
-
       setIsAuth(false);
       router.push("/auth/signin");
     }
